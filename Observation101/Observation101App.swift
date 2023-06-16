30bd227b3398f212c686a11b815c3d939e712220
Andreas Seeger <splash@appfros.ch>
2023-06-16 11:19:05 +0200

    Initial Commit

---
+++ Observation101/Observation101App.swift
@@ -0,0 +1,17 @@
+//
+//  Observation101App.swift
+//  Observation101
+//
+//  Created by Andreas Seeger on 16.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Observation101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
