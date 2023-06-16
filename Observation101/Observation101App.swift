88193d61d126ab9f7f9b4d6378a6853dbf11be75
Andreas Seeger <splash@appfros.ch>
2023-06-16 11:34:52 +0200

    REFACTOR consolidated all code to one file for better "copiability"

--- Observation101/Observation101App.swift
+++ Observation101/Observation101App.swift
@@ -15,3 +15,15 @@ struct Observation101App: App {
         }
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Observation101/Temp.swift
@@ -0,0 +1,8 @@
+//
+//  Temp.swift
+//  Observation101
+//
+//  Created by Andreas Seeger on 16.06.23.
+//
+
+import Foundation
